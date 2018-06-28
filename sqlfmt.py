bad3ea0dca1de0f12d4959dcfafe1ef91e1c27c9
zxy <orcexzy@163.com>
2018-06-28 10:43:29 +0800

    product

---
+++ sqlfmt.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018/6/26
+
+@author: xing yan
+"""
+
+bp_fund ="insert into bp_ta_info (TA_CODE, TA_NAME, TA_ABBR, TA_STAT, CUST_INFO_TYPE_ID, REC_STAT, CHECK_FLAG, CREATOR, CHECKER, MODIFIER, CRE_DT, MOD_DT, MARGIN_PMT_FLAG, PART_CODE, DFLT_CHK_MODE_05, DFLT_CHK_MODE_26, BAL_DTL_26_DEAL_FLAG, INTERFACE_TYPE, SUPPORT_CARD_TYPE, DEFINED_COMPANY_CODE) " \
+         "values ('T2', '私募代销测试TA', '私募代销测试TA', '0', '0001', '0', '1', 'may.tang', 'may.tang', '', '20180401', '', '', '', '2', '1', '1', '1', '3', '');"
